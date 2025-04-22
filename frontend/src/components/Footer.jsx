1064ed3b9437e70a0b68988f2dfbf9bddb18386e
Alejandro Rodriguez <a.rodriguez727@icloud.com>
2025-04-22 00:01:20 -0600

    added footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        📅 Predictions are updated daily at 1:00 PM Eastern Time (11:00 AM Mountain Time)
+      </footer>
+    );
+  }
+  
